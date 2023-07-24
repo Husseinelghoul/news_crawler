aaecfc0c179470c6b428b0b1295304ea83001693
Hussein <hussein@quiqup.com>
2023-07-24 11:10:45 +0300

    Adding linting changes

--- news_api/Article.py
+++ news_api/Article.py
@@ -1,6 +1,8 @@
 """
 article.py - Defines the Article class for representing news articles.
 """
+from __future__ import annotations
+
 import hashlib
 
 
@@ -9,16 +11,16 @@ class Article:
     Represents a news article.
 
     Attributes:
-        id (int): The unique identifier for the article, generated based on the hash of the URL.
+        id (str): The unique identifier for the article, generated based on the hash of the URL.
         headline (str): The main headline of the article.
         url (str): The URL of the article.
-        label (Optional[str]): The label/category of the article (default is None).
-        title (Optional[str]): The title of the article (default is None).
-        subtitle (Optional[str]): A subheading or additional information about the article (default is None).
-        author (Optional[str]): The author of the article (default is None).
-        about_the_author (Optional[str]): Information about the author's background and expertise (default is None).
-        datetime_string (Optional[str]): The date and time of the article in string format (default is None).
-        article_body (Optional[str]): The main content/body of the article (default is None).
+        label (str | None): The label/category of the article (default is None).
+        title (str | None): The title of the article (default is None).
+        subtitle (str | None): A subheading or additional information about the article (default is None).
+        author (str | None): The author of the article (default is None).
+        about_the_author (str | None): Information about the author's background and expertise (default is None).
+        datetime_string (str | None): The date and time of the article in string format (default is None).
+        article_body (str | None): The main content/body of the article (default is None).
 
     Methods:
         __init__(self, headline: str, url: str) -> None:
@@ -34,6 +36,17 @@ class Article:
             The official representation is the headline of the article.
     """
 
+    id: str
+    headline: str
+    url: str
+    label: str | None
+    title: str | None
+    subtitle: str | None
+    author: str | None
+    about_the_author: str | None
+    datetime_string: str | None
+    article_body: str | None
+
     def __init__(self, headline: str, url: str) -> None:
         """
         Initializes an Article object with the provided headline and URL.
@@ -44,7 +57,7 @@ class Article:
             url (str): The URL of the article.
         """
         hash_object = hashlib.sha1(url.encode("utf-8"))
-        self.id = int(hash_object.hexdigest(), 16)
+        self.id = str(hash_object.hexdigest())
         self.headline = headline
         self.url = url
 
