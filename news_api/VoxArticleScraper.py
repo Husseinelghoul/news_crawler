715d0a8a21ff8443facbfa9a92b2f518e1457fc4
Hussein <hussein@quiqup.com>
2023-07-24 13:10:44 +0300

    Improved ArticleScraper

--- news_api/VoxArticleScraper.py
+++ news_api/VoxArticleScraper.py
@@ -25,6 +25,7 @@ class VoxArticleScraper:
     Attributes:
         base_url (str): The base URL of the Vox news website.
         articles_list (list[Article]): A list to store scraped Article objects.
+        time_out (int) : request time out in seconds
 
     Methods:
         __init__(self) -> None:
@@ -38,7 +39,7 @@ class VoxArticleScraper:
             Populates additional attributes of Article objects by scraping individual article pages.
             Returns the updated articles_list.
 
-        __get_label(self, label: Tag) -> str:
+        __get_label(self, label: Tag, Tag, article: Article) -> Union[str, None]:
             Helper method to extract and clean the article label from the HTML soup.
 
         __get_author_bio(self, about_the_author: Tag, article: Article) -> Union[str, None]:
@@ -48,10 +49,14 @@ class VoxArticleScraper:
         __get_article_body(self, article_body: Tag) -> str:
             Helper method to extract and clean the article body text from the HTML soup.
             Removes any footer items and replaces multiple newlines with one newline.
+        __get_article_datetime(self, article_datetime_string: Tag, article: Article) -> Union[str, None]:
+            Helper method to extract return the article's datetime from the HTML soup.
+            Returns either the article's datetime as a string or None if not found.
     """
 
     base_url: str = BASE_URL
     articles_list: list[Article] = []
+    time_out: int = 30
 
     def __init__(self) -> None:
         """
@@ -72,9 +77,9 @@ class VoxArticleScraper:
         Returns:
             list[Article]: The list of Article objects representing the scraped articles.
         """
-        for i in range(1, 2):  # TODO: This is supposed to be 15
+        for i in range(1, 15):
             url = self.base_url + ("" if i == 1 else str(i))
-            response = requests.get(url, timeout=10)
+            response = requests.get(url, timeout=self.time_out)
             soup = BeautifulSoup(response.content, "html.parser")
             entries = soup.find_all("div", class_="c-compact-river__entry")
             for entry in entries:
@@ -98,30 +103,38 @@ class VoxArticleScraper:
         for article in self.articles_list:
             logger.debug("getting article '%s'", article.headline)
             logger.debug("requesting url %s", article.url)
-            response = requests.get(article.url, timeout=10)
+            response = requests.get(article.url, timeout=30)
             soup = BeautifulSoup(response.content, "html.parser")
-            article.label = self.__get_label(soup.find("div", class_="c-entry-group-labels"))
-            article.title = soup.find("h1", class_="c-page-title").text
+            article_title = soup.find("h1", class_="c-page-title")
+            # if the title isn't clear it's not an article but a collection of articles
+            if not soup.find("h1", class_="c-page-title"):
+                continue
+            article.title = article_title.text
+            article.label = self.__get_label(soup.find("div", class_="c-entry-group-labels"), article)
             article.subtitle = soup.find("p", class_="c-entry-summary p-dek").text
             article.author = soup.find("span", class_="c-byline__author-name").text
             article.about_the_author = self.__get_author_bio(
                 soup.find("div", class_="c-short-author-bio-wrapper"), article
             )
-            article.datetime_string = soup.find("time", {"data-ui": "timestamp"})["datetime"]
+            article.datetime_string = self.__get_article_datetime(soup.find("time", {"data-ui": "timestamp"}), article)
             article.article_body = self.__get_article_body(soup.find("div", class_="c-entry-content"))
         return self.articles_list
 
-    def __get_label(self, label: Tag) -> str:
+    def __get_label(self, label: Tag, article: Article) -> str | None:
         """
         Helper method to extract and clean the article label from the HTML soup.
 
         Args:
             label (Tag): The HTML tag containing the article label.
+            article (Article): The Article object representing the article.
 
         Returns:
-            str: The cleaned article label.
+            Union[str, None]: The cleaned tag as a string, or None if not found.
         """
-        return " ".join(label.text.replace("\n", " ").replace("Filed under: ", "").split())
+        if label:
+            return " ".join(label.text.replace("\n", " ").replace("Filed under: ", "").split())
+        logger.debug("no label found for article '%s', url:%s", article.headline, article.url)
+        return None
 
     def __get_author_bio(self, about_the_author: Tag, article: Article) -> str | None:
         """
@@ -156,3 +169,19 @@ class VoxArticleScraper:
             footer_item.extract()
         # replace chained new lines with one new line
         return re.sub(r"\n+", "\n", article_body.text)
+
+    def __get_article_datetime(self, article_datetime_string: Tag, article: Article) -> str | None:
+        """
+        Helper method to extracts datetime as string from the HTML soup.
+
+        Args:
+            article_datetime_string (Tag): The HTML tag containing the article's datetime.
+            article (Article): The Article object representing the article.
+
+        Returns:
+            Union[str, None]: The datetime as a string, or None if not found.
+        """
+        if article_datetime_string:
+            return str(article_datetime_string["datetime"])
+        logger.debug("no datetime found for article '%s', url:%s", article.headline, article.url)
+        return None
